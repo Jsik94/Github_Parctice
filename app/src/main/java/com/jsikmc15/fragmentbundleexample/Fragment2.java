ce6af3137e84aafdcf715f8e7fbe3250ce47b7ac
JSik <ykmgameha@gmail.com>
2021-09-26 16:01:49 +0900

    Third2

--- app/src/main/java/com/jsikmc15/fragmentbundleexample/Fragment2.java
+++ app/src/main/java/com/jsikmc15/fragmentbundleexample/Fragment2.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -23,7 +25,7 @@ public class Fragment2 extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+        ArrayList<String> lst ;
         view = inflater.inflate(R.layout.fragment_2,container,false);
 //        view = getLayoutInflater().inflate(R.layout.fragment_2,container,false);
         tv_tvfrag2 = view.findViewById(R.id.tv_frag2);
